02b620df2dd8579f3bc83c59a4fae729f31a43ef
Ziyodulla Jabborov <shlmziyo@gmail.com>
2023-07-27 15:11:29 +0200

    Adding POST request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 // const dummyMovies = [
 //   {
@@ -46,20 +47,35 @@ const [error, setError] = useState(null);
 
 const fetchMoviesHandler = useCallback(async() => {
   setIsLoading(true);
+  setError(null);
 
   try {
-    const response = await fetch('https://swapi.dev/api/films/');
+    const response = await fetch('https://movie-list-22d8e-default-rtdb.europe-west1.firebasedatabase.app/movie.json');
+      if(!response.ok){
+        throw new Error('Something went wrong !!!');
+      };
+
     const data = await response.json();
-      
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
+        // const transformedMovies = data.results.map((movieData) => {
+        //   return {
+        //     id: movieData.episode_id,
+        //     title: movieData.title,
+        //     openingText: movieData.opening_crawl,
+        //     releaseDate: movieData.release_date,
+        //   };
+        // });
+
+        const loadedMovies = [];
+        for (const key in data) {
+          loadedMovies.push(
+            {
+              id: key,
+              title: data[key].title,
+              openingText: data[key].openingText,
+              releaseDate: data[key].releaseDate,
+            });
+        };
+        setMovies(loadedMovies);
   } catch (error) {
     setError(error.message);
   }
@@ -70,6 +86,21 @@ const fetchMoviesHandler = useCallback(async() => {
       fetchMoviesHandler();
     }, [fetchMoviesHandler])
 
+    const addMovieHandler = async(movie) => {
+      try {
+        const response = await fetch('https://movie-list-22d8e-default-rtdb.europe-west1.firebasedatabase.app/movie.json', {
+          method: 'POST',
+          body: JSON.stringify(movie),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+        });
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
     let content = <p>Found no movies.</p>;
 
@@ -87,6 +118,9 @@ const fetchMoviesHandler = useCallback(async() => {
   
     return (
       <React.Fragment>
+        <section>
+          <AddMovie onAddMovie={addMovieHandler}/>
+        </section>
         <section>
           <button onClick={fetchMoviesHandler}>Fetch Movies</button>
         </section>
